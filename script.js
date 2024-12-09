b1d21b7c48b1864b824b8082b69c46dfc9d606f0
leelachouhan23 <leelachouhan23@navgurukul.org>
2024-12-09 13:01:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const bodyElement = document.body
+const div = document.createElement('div');
+const heading = document.createElement("h1");
+bodyElement.appendChild(div);
+// const heading = document.createElement("h1")
+heading.innerText = "Hello World"
+div.appendChild(heading);
+console.log(bodyElement);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
